7d06ea1db9eeb2439acd7ba7e08ed71ca5f8a7ba
Mansi <mansivaghasiya0302@gmail.com>
2022-08-29 09:42:48 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider_api_calling/provider/dataModel_provider.dart';
-import 'package:provider_api_calling/view/api_view.dart';
-import 'package:provider/provider.dart';
+import 'package:provider_api_calling/myApp.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => DeerProvider(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const BeerApiView(),
-      ),
-    );
-  }
-}
---
+++ lib/myApp.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_api_calling/provider/dataModel_provider.dart';
+import 'package:provider_api_calling/view/api_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => DeerProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const BeerApiView(),
+      ),
+    );
+  }
+}
